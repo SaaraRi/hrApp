27a97b05107bdcb128be19dace973daa0472ef16
SaaraRi <saara.rikama@gmail.com>
2025-04-08 09:36:32 +0300

    step1 changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Person from './components/Person';
+import { persons } from './personsData';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ <div>
+      <Header logo="HR Manager App" />
+    </div>
+    <main>
+      {persons.map((person) => (
+          <Person 
+            key={person.id}
+            name={person.name}
+            title={person.title}
+            salary={person.salary}
+            phone={person.phone}
+            email={person.email}
+            animal={person.animal}
+          />
+      ))}
+    </main>
+    <div>
+      <Footer year={2025} />
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Person.jsx
@@ -0,0 +1,15 @@
+
+const Person = (props) => {
+    return (
+        <div className="box">
+            <p>Name: {props.name}</p>
+            <p>Title: {props.title}</p>
+            <p>Salary: {props.salary}</p>
+            <p>Phone: {props.phone}</p>
+            <p>Email: {props.email}</p>
+            <p>Animal: {props.animal}</p>
+        </div>
+    );
+};
+
+    export default Person;
